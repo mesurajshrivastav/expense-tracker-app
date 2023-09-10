69d8ce09a811c8784a834608f3c5cfc56eac0739
Suraj Shrivastav <109119656+mesurajshrivastav@users.noreply.github.com>
2023-09-10 13:55:32 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -9,17 +9,17 @@ const transactionsRoute = require('./routes/transactionsRoute');
 
 
 
+
 app.use('/api/users/', userRoute);
 app.use('/api/transactions/', transactionsRoute);
 
+//static files
+app.use(express.static(path.join(__dirname, '../client/build')))
+
+app.get('*', function(req,res) {
+  res.sendFile(path.join(__dirname, '../client/build'));
+})
+
 const port = process.env.PORT || 5000;
-
-if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static('../client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
-  });
-}
 
 app.listen(port, () => console.log(`Node JS Server started at port ${port}!`));
