b29321bc3944f78e53ac59a09d1cdb5184d25f8f
Suraj Shrivastav <109119656+mesurajshrivastav@users.noreply.github.com>
2023-09-10 13:25:30 +0530

    final code
    mern app deployment

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const dbConnect = require('./dbConnect');
+const app = express();
+app.use(express.json());
+const path = require('path');
+const userRoute = require('./routes/usersRoute');
+const transactionsRoute = require('./routes/transactionsRoute');
+const path = require('path')
+
+
+
+
+app.use('/api/users/', userRoute);
+app.use('/api/transactions/', transactionsRoute);
+
+//static files
+app.use(express.static(path.join(__dirname, './client/build')))
+
+app.get('*', function(req,res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+})
+
+//port
+const port = process.env.PORT || 5000;
+
+
+app.listen(port, () => console.log(`Node JS Server started at port ${port}!`));
